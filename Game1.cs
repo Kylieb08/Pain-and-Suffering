ec816287b328693dc3f43f91718cfb11fd53df96
Kylie Banman <339780520@GoTVDSB.ca>
2025-06-04 14:35:15 -0400

    fixed namespace

--- Game1.cs
+++ Game1.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace Skeleton_Game
+namespace Pain_and_Suffering
 {
     public class Game1 : Game
     {
